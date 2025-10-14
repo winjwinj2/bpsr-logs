0e1f45fae7c13b5738c1827335e735e0d2018c0b
winjwinj <winstonjodjana@gmail.com>
2025-10-14 16:12:49 -0700

    small bugfix for names having leading character

--- src-tauri/src/live/opcodes_process.rs
+++ src-tauri/src/live/opcodes_process.rs
@@ -223,7 +223,7 @@ pub fn process_aoi_sync_delta(
 
 fn process_player_attrs(player_entity: &mut Entity, target_uid: i64, attrs: Vec<Attr>) {
     for attr in attrs {
-        let Some(raw_bytes) = attr.raw_data else {
+        let Some(mut raw_bytes) = attr.raw_data else {
             continue;
         };
         let Some(attr_id) = attr.id else { continue };
@@ -232,7 +232,9 @@ fn process_player_attrs(player_entity: &mut Entity, target_uid: i64, attrs: Vec<
         match attr_id {
             attr_type::ATTR_NAME => {
                 // todo: fix these brackets
-                player_entity.name = BinaryReader::from(raw_bytes).read_string().unwrap();
+                raw_bytes.remove(0); // not sure why, there's some weird character as the first e.g. "\u{6}Sketal"
+                let player_name = BinaryReader::from(raw_bytes).read_string().unwrap();
+                player_entity.name = player_name;
                 info! {"Found player {} with UID {}", player_entity.name, target_uid}
             }
             #[allow(clippy::cast_possible_truncation)]
